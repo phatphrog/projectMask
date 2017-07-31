3c230fbb3e8e494ae2d4085ae7f83fac17ea346e
Marcel Terblanche <phatphrog@gmail.com>
2017-08-01 01:44:03 +0200

    added walk cycle and idle animations to player. fixed weight painting on player model. improved walk cycle animation.

---
+++ Assets/Scripts/WalkAnimation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WalkAnimation : MonoBehaviour {
+
+    private Animator anim;
+    private float vertInput;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        vertInput = Input.GetAxis("Vertical");
+        anim.SetFloat("walk", vertInput); //set transition to start playing walk animation
+	}
+}
